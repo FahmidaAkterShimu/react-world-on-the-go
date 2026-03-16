c7dba53783c221fecebcd141b6dccab57a433418
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-03-16 16:09:13 +0600

    Show flags and other information, and handle data error

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Country = () => {
+const Country = ({ country }) => {
     return (
         <div>
-            <h3>Name: </h3>
+            <img src={country.flags?.flags?.png} alt={country.flags.flags.png} />
+            <h3>Name: {country.name.common}</h3>
+            <h4>Capital: {country.capital.capital[0]}</h4>
+            <p>Region: {country.region.region}</p>
+            <p>Area: {country.area.area}</p>
+            <p>Population: {country.population.population}</p>
         </div>
     );
 };
