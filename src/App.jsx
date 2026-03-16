6813c6ba2a397209e44da7327fb30c498f4c0cfe
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-03-16 15:40:29 +0600

    Display countries in a simple way

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { Suspense } from 'react'
 import './App.css'
 import Countries from './components/Countries/Countries'
+
+
+const countriesPromise = fetch("https://openapi.programming-hero.com/api/all").then(res => res.json())
 
 function App() {
 
   return (
     <>
-      <div>
-       <Countries></Countries>
-      </div>
+      <Suspense fallback={<p>Country details loading...</p>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
     </>
   )
 }
